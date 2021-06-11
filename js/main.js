8c3a9e8a57830b25fcb3638c24076e96756b2031
Victorr <2020003498@ifam.edu.br>
2021-06-10 22:11:47 -0400

    ok

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/*var nome = "Victor Duarte";
+var idade = 20;
+var idade2 = 10;
+var frase = "Japao é o melhor time do mundo!"
+alert(nome+" tem "+idade+" anos!");
+for(var i = 0; i <= 5; i++){
+    idade2++;
+    alert(idade2);
+    if(idade2 == 15){
+        alert("Novo valor da idade 2: "+idade2);
+    }
+}*/
+
+/*for(var i = 0; i <= 4; i++){
+    idade2++;
+    console.log(idade2);
+    if(idade2 == 15){
+        console.log("Novo valor da idade 2: "+idade2);
+    }
+}*/
+
+/*console.log(frase.replace("Japao", "Brasil"));
+alert(frase.replace("Japao", "Brasil"));
+console.log(frase.toUpperCase());*/
+
+/*var lista = ['Maçã', 'Pera', 'Laranja', 'Tomate'];
+lista.push("Uva");
+lista.pop("Uva");
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
+
+/*var fruta = [
+    {nome: "maçã", cor: "vermelho"}, 
+    {nome: "uva", cor: "roxa"}, 
+    {nome: "banana", cor: "amarelo"}]
+console.log(fruta.cor);
+console.log(fruta.nome);*/
+
+var idade = 18;
+if (idade >= 18){
+    alert("Maior de idade!");
+}else{
+    alert(Menor de idade!);
+}
